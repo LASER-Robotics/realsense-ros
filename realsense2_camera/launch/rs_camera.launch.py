45ca25f72dc622db9917dd80a7f162f2d082d9be
Augusto-Viniciuss <augustoviniciud@gmail.com>
2025-10-28 10:46:40 -0300

    remove static tf of launch rs_camera

--- realsense2_camera/launch/rs_camera.launch.py
+++ realsense2_camera/launch/rs_camera.launch.py
@@ -18,11 +18,6 @@ def launch_setup(context: LaunchContext):
     camera_name = LaunchConfiguration('camera_name')
     camera_params_file = LaunchConfiguration('camera_params_file')
 
-    fcu_frame = uav_name.perform(context) + '/fcu'
-    fcu_frame_slashless = 'fcu_' + uav_name.perform(context)
-
-    realsense_frame = uav_name.perform(context) + '/' + camera_name.perform(context) + '/link'
-    realsense_frame_slashless = uav_name.perform(context) + '_' + camera_name.perform(context) + '_link'
 
     # Declare nodes
     realsense_camera_node = Node(
@@ -34,24 +29,7 @@ def launch_setup(context: LaunchContext):
         parameters=[ParameterFile(camera_params_file, allow_substs=True),
                     {'use_sim_time': use_sim_time}])
 
-    # TODO(anyone): how can I get the pose of the camera for each frame?
-    fcu_to_realsense_tf_static_publisher_node = Node(
-        package='tf2_ros',
-        executable='static_transform_publisher',
-        name=TextSubstitution(text=fcu_frame_slashless + '_to_' + realsense_frame_slashless),
-        namespace=uav_name,
-        output='screen',
-        parameters=[{'use_sim_time': use_sim_time}],
-        arguments=['--x', '0.0',
-                   '--y', '0.0',
-                   '--z', '0.15',
-                   '--yaw', '0.0',
-                   '--pitch', '0.0',
-                   '--roll', '0.0',
-                   '--frame-id', fcu_frame,
-                   '--child-frame-id', realsense_frame]) # NOTE: x500 frame
-
-    return [realsense_camera_node, fcu_to_realsense_tf_static_publisher_node]
+    return realsense_camera_node
 
 
 def generate_launch_description():
